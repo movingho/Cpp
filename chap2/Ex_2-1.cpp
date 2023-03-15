25493f2da3ca45ac4b4426702263180a27602f32
movingho <126740002+movingho@users.noreply.github.com>
2023-03-15 13:46:53 +0900

    Add files via upload

---
+++ chap2/Ex_2-1.cpp
@@ -0,0 +1,13 @@
+/*
+    소스: SimpleC++.cpp
+    cout과 << 연산자를 이용하여 화면에 출력한다.
+*/
+
+#include <iostream> // cout과 << 연산자 포함
+using namespace std; // 매번 std:: 을 써야하는 귀찮음 해결
+// C++ 프로그램은 main() 함수에서부터 실행을 시작한다.
+int main() {
+    cout << "Hello" << endl; // 화면에 Hello를 출력하고 다음 줄로 넘어감
+    cout << "첫 번째 맛보기입니다.\n";
+    return 0; // main() 함수가 종료하면 프로그램이 종료됨
+}
